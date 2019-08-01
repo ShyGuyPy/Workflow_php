c57a7e862b0ff8a661fc88f88b838dcd0167af36
ShyGuyPy <luke.vawter1@gmail.com>
2019-08-01 15:46:28 -0400

    working through somre of sarah's code

--- test.php
+++ test.php
@@ -1,5 +1,17 @@
 
 <?php
+
+function test_function($test_para){
+echo"This script is $test_para";
+}
+
 $testing = "working";
-echo"This script is $testing";
+
+$run=1;
+  if($run){
+    $testing = "for testing";
+  }
+
+test_function($testing)
+//echo"This script is $testing";
 ?>
