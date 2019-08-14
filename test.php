611eac244eeb87125f3c9221b8c0c894f01da097
ShyGuyPy <luke.vawter1@gmail.com>
2019-08-14 17:25:05 -0400

    phpexcel script not yet working in Drupal site

--- test.php
+++ test.php
@@ -1,12 +1,14 @@
 
 <?php
-
 function test_function($test_para){
+for($i=1;$i <= 5; $i++){
 echo"This script is $test_para";
+echo"___"; }
 }
 
 $testing = "working";
 
+//$run = 0;
 $run=1;
   if($run){
     $testing = "for testing";
@@ -14,4 +16,5 @@ $run=1;
 
 test_function($testing)
 //echo"This script is $testing";
+
 ?>
