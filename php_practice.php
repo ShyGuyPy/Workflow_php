8aee33b8c441adc27c64d49a9294af9387837220
ShyGuyPy <luke.vawter1@gmail.com>
2019-08-21 17:14:52 -0400

    working through test script

---
+++ php_practice.php
@@ -0,0 +1,12 @@
+<?php
+
+//multi dimensional array
+$test_array = array(
+  array("test1",1,11,111),
+  array("test2",2,22,222),
+  array("test3",3,33,333)
+);
+
+echo"index item 2 is:".$test_array[0][2]
+
+?>
