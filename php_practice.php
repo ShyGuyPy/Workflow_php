3ca5ce6cbef688c7b7c0ae5a713805aa286c00b6
ShyGuyPy <luke.vawter1@gmail.com>
2019-08-22 17:28:45 -0400

    constants, objects, concats, string counting funcs

--- php_practice.php
+++ php_practice.php
@@ -7,6 +7,27 @@ $test_array = array(
   array("test3",3,33,333)
 );
 
-echo"index item 2 is:".$test_array[0][2]
+echo"index item 2 is:".$test_array[0][2];
+
+echo "<br>";
+//create on object
+class test_ob{
+  public $test_var = "testing class properties";
+
+  function show_test_var(){
+    echo "func works";
+    //return $this->test_var;
+  }
+}
+
+$instance_test = new test_ob;
+$instance_test->show_test_var();
+
+$random_test_var = "this is only a test";
+
+echo "<br>";
+echo strlen($random_test_var);
+echo "<br>";
+echo str_word_count($random_test_var);
 
 ?>
