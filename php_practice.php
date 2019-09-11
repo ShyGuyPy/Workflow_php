695b8765f2d7c8e327578916e2d302bad087a803
ShyGuyPy <luke.vawter1@gmail.com>
2019-09-11 14:59:37 -0400

    loops on arrays of different types, require_once, other stuff

--- php_practice.php
+++ php_practice.php
@@ -1,23 +1,30 @@
 <?php
 
 //multi dimensional array
-$test_array = array(
+$test_array = array(1,11,111);
+
+$test_array2 = array(
   array("test1",1,11,111),
   array("test2",2,22,222),
   array("test3",3,33,333)
 );
 
-define("test_con", 2);
+$test_array3 = array(
+  "array1" =>array(
+    "name"=>1,"age"=>11, "number"=>111,
+  ),
+  "array2" =>array(
+    "name"=>2,"age"=>22, "number"=>222,
+  ),
+  "array3" =>array(
+    "name"=>3,"age"=>33, "number"=>333,
+  )
+);
 
-echo"index item 2 is:".$test_array[0][test_con];
+echo"index item 2 is:".$test_array[0][2];
 
-echo "<br>";
 
-foreach ($test_array as $value) {
-  echo "$value";
-}
 
-echo "<br>";
 //create on object
 class test_ob{
   public $test_var = "testing class properties";
@@ -37,9 +44,9 @@ echo "<br>";
 echo strlen($random_test_var);
 echo "<br>";
 echo str_word_count($random_test_var);
+echo "<br>";
 
-
-
-
+$random_test_var2 = " of a concat assignment";
+echo $random_test_var.=$random_test_var2;
 
 ?>
--- test_9_10_2019.php
+++ test_9_10_2019.php
@@ -1,3 +1,37 @@
 <?php
-require_once '....';
+require_once 'php_practice.php';
+
+
+
+define("test_con", 2);
+
+echo"index item 2 is:".$test_array[0][test_con];
+echo "<br>";
+
+echo "<br>";
+
+foreach ($test_array as $value) {
+  echo "$value <br>";//"item in list <br>";
+}
+
+echo "<br>";
+
+foreach ($test_array2 as $value) {
+  //echo "$value";
+  foreach($value as $sub_value) {
+    echo "$sub_value <br>";
+  }
+}
+echo "<br>";
+
+$keys =array_keys($test_array3);
+for($i = 0; $i < count($test_array3); $i++) {
+  echo $keys[$i] . "{<br>";
+  foreach($test_array3[$keys[$i]] as $key => $value) {
+    echo $key . " : " . $value . "<br>";
+  }
+  echo "}<br>";
+}
+
+
 ?>
